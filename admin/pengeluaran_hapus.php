45896dc72174b6b2117a7d44293ee8c018460e07
E31240791_Awanda Rizatur Rohim <daaaracing@gmail.com>
2025-10-31 10:52:07 +0700

    form admin 1

--- admin/pengeluaran_hapus.php
+++
@@ -1,9 +0,0 @@
-<?php
-require_once __DIR__ . '/../config/database.php';
-
-$id = $_GET['id'];
-mysqli_query($conn, "DELETE FROM pengeluaran WHERE id_pengeluaran='$id'");
-$_SESSION['toast_success'] = 'Data pengeluaran berhasil dihapus!';
-header('Location: pengeluaran.php');
-exit;
-?>
